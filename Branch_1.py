65aad7ed38e357c7823ff778dcc54ac33239f792
AleKolar <alekolar17982@gmail.com>
2024-03-30 11:06:48 +0300

    Try_other_way2

--- Branch_1.py
+++ Branch_1.py
@@ -11,12 +11,15 @@ class Board:
         self.board = [int(i) for i in range(0, 10)]
         self.used = False
 
+
+
     def create_board(self):  # создаем игровое поле
         print(self.board[1], ' | ', self.board[2], ' | ', self.board[3])
         print('_' * 14)
         print(self.board[4], ' | ', self.board[5], ' | ', self.board[6])
         print('_' * 14)
         print(self.board[7], ' | ', self.board[8], ' | ', self.board[9])
+
 
     def check_ceil_empty(self):  # ф-ия проверяет занятость позиции
         if self.board[self.position] == '':
@@ -54,13 +57,24 @@ class Player(Board):
             while position not in [1, 2, 3, 4, 5, 6, 7, 8, 9] and self.board[self.position] != '':
                 position = int(input("Укажите поле от 1 до 9:"))
                 self.used = True
-                self.position = position
-                return self.set_marker()
+                return position
             else:
                 self.check_full_fill()
 
-    def set_marker(self):
+    @property
+    def cell(self):
+        return self.board[self.position]
+
+    @cell.setter
+    def cell(self, position):
+        self.choice_player()
+        self.position = position
+
+    def set_board(self):
         self.board[self.position] = self.marker
+
+    '''def set_marker(self):
+        self.board[self.position] = self.marker'''
 
     def game(self):
         while self.check_full_fill() == False:
